1e6f93936619e98c090cc9f2fbcfa7ff945d82f5
kk@skywifi.pro <kkuznetsov>
2017-09-04 03:26:46 +0300

    ver

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='skyagent',
-      version='0.1',
+      version='0.2',
       description='Skywifi agent',
       url='https://github.com/Wounderer/skyagent.git',
       author='SkyWifi',
       author_email='kk@skywifi.pro',
       license='MIT',
       packages=['skyagent'],
-      scripts=['bin/skyagent'],
+      scripts=['bin/skyagent','bin/sky_env'],
       install_requires=[
           'twisted',
           'txaio',
